61f72bc9fa476d46177b8fb240e335304d631bae
B13L <48735787+b13l@users.noreply.github.com>
2026-03-17 00:12:48 -0300

    primeiro upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'diaz-pdv-v2';
+const ASSETS = [
+  './DiazDelicias_PDV.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return resp;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
